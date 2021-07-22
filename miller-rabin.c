ed73b0ec23118e72cef150f53ca7e7c677e725ad
jatewon <84976508+jatewon@users.noreply.github.com>
2021-07-22 16:09:25 +0700

    Update miller-rabin.c

--- miller-rabin.c
+++ miller-rabin.c
@@ -1,94 +1,81 @@
-#include <stdio.h>
-#include <math.h>
-#include <stdlib.h>
-
-int n , t = 0, s, mang[50], A, p, a;
+#include<stdio.h>
+#include<math.h>
+int n,t,s,mang[100],b,a,p;
 int k = 0;
 void nhap()
 {
-    while (n < 3)
+    while(n<3)
     {
-        printf("Nhap n: ");
-        scanf("%d", &n);
+        printf("n");
+        scanf("%d",&n);
     }
-
-    while (t < 1)
+    while(t<1)
     {
-        printf("\nnhap t: ");
-        scanf("%d", &t);
+        printf("t");
+        scanf("%d",&t);
     }
 }
-int tim_s(int k){
+int tims(int k)
+{
     int r;
-    int x =k-1;
-    while(r %2 ==0){
-        r= x / 2;
-        x= x/2;
+    int x = k-1;
+    while(r%2==0)
+    {
+        r = x/2;
+        x=x/2;
     }
-    s= log((k-1) / r) / log(2);
+    s = log((k-1)/r)/log(2);
     return s;
 
 }
-
-int binhPhuong(int x, int v)
+int binhphuong(int m,int v)
 {
-
-    while (x != 0)
+    while(m!=0)
     {
-        mang[k] = x % 2;
-        x = x / 2;
+        mang[k]=m%2;
+        m = m/2;
         k++;
     }
     p = 1;
-    for (int i = k - 1; i >= 0; i--)
+    for(int i =k-1; i>=0; i--)
     {
-        A = (p * p) % n;
-        if (mang[i] == 1)
+        b = (p*p)%n;
+        if(mang[i]==1)
         {
-            p = (v * A) % n;
+            p = (b*a)%n;
         }
-        else if (mang[i] == 0)
+        else if(mang[i]==0)
         {
-            p = A;
+            p = b;
         }
     }
     return p;
 }
-void tinh()
+int main()
 {
     nhap();
-    tim_s(n);
-    for (int i = 1; i <= t; i++)
+    tims(n);
+    for(int i = 1; i<=t; i++)
     {
-        a = 2 + rand() % ((n - 2) + 1 - 2);
-        printf("\n voi a= %d thi ", a);
-        binhPhuong(n - 1, a);
-        if (p != 1 && p != (n - 1))
+        a = 2 + rand()%((n-2)+1-2);
+        binhphuong(n-1,a);
+        if(p!=1 && p !=(n-1))
         {
             int j = 1;
-            while (j <= (s - 1) && p != (n - 1))
+            while(j <(s-1) && p != (n-1))
             {
-                p = p * p % n;
-                if (p == 1)
-                {
-                    printf("hop so");
-                }
-                j += 1;
-            }
-            if (p != (n - 1))
-            {
-                printf("hop so");
+                p = p*p%n;
+                j+=1;
             }
         }
-        else
+    }
+    if(p ==1)
         {
             printf("nguyen to");
         }
-    }
-}
-int main()
-{
-
-    tinh();
+        else
+        {
+            printf("hop so");
+        }
     return 0;
 }
