91b4b164881da87375233490262633af94d00eb9
jatewon <84976508+jatewon@users.noreply.github.com>
2021-07-23 08:25:17 +0700

    Update boyer-moore.c

--- boyer-moore.c
+++ boyer-moore.c
@@ -1,40 +1,63 @@
 #include<stdio.h>
 #include<math.h>
-#include<string.h>
 char t[100],p[100];
+int l[150];
 void nhap()
 {
     printf("T");
-    scanf("%s",&t);
-    printf("P");
-    scanf("%s",&p);
+    fgets(t,sizeof(t),stdin);
+    printf("p");
+    fgets(p,sizeof(p),stdin);
+    for(int i = 0;i<150;i++)
+    {
+        l[i]=-1;
+    }
+    for(int i = 0;i<(int)(strlen(t)-1);i++)
+    {
+        l[(int)t[i]]=i;
+    }
 }
-void vetcan()
+int kiemtra()
 {
     int d= 0;
-    for(int i =0;i<strlen(t);i++)
+    int j = strlen(p)-2,congthem = 0,i= strlen(p)-2;
+    while(i<strlen(t))
     {
-        d = 0;
-        for(int j = 0;j<strlen(p);j++)
+        if(congthem !=0)
         {
-            if(t[i+j]==p[j])
-            {
-                d++;
-            }
+            d = 0;
+            i = i +congthem;
+            j = strlen(p)-2;
         }
-        if(d == (int)(strlen(p)))
+        if(t[i]==p[j])
         {
-            printf("vi tri bat dau la %d",i+1);
-            break;
-        } else if(i == (int)(strlen(t)-1) && d<(int)(strlen(p)))
+            i--;
+            j--;
+            d++;
+            congthem = 0;
+        }else{
+            int min;
+            if(j >1 + l[(int)t[i]])
+            {
+                min = 1 + l[(int)t[i]];
+            }else
+            {
+                min = j;
+            }
+            congthem = (strlen(p)-1)-min;
+        }
+        if(d  == (int)strlen(p)-1)
         {
-            printf("khong co trong T");
+            printf("P co trong T o vi tri %d",i);
+            return 0;
         }
     }
+    printf("ko co trong T");
 }
 int main()
 {
     nhap();
-    vetcan();
+    kiemtra();
+    getchar();
     return 0;
 }
