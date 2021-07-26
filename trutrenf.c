a43212009d558e7a4aed2ea1232ef6140ae6997a
jatewon <84976508+jatewon@users.noreply.github.com>
2021-07-26 20:31:00 +0700

    Update trutrenf.c

--- trutrenf.c
+++ trutrenf.c
@@ -1,4 +1,3 @@
-#include<stdio.h>
 #include <stdio.h>
 #include <math.h>
 
@@ -7,50 +6,15 @@ long long p, d, e, a[100], b[100];
 
 void nhap()
 {
-    int k;
     printf("nhap p = ");
     scanf("%lld", &p);
     printf("nhap w = ");
     scanf("%d", &w);
-
-    while (1)
-    {
-        printf("\n\nban muon nhap nhu the nao??");
-        printf("\nchon 1 hoac 2");
-        printf("\n1. nhap so");
-        printf("\n2. nhap mang");
-        printf("\nban chon = ");
-        scanf("%d", &k);
-
-        if (k == 1)
-        {
-            printf("nhap a = ");
-            scanf("%lld", &d);
-            printf("nhap b = ");
-            scanf("%lld", &e);
-            tru_tren_f(k);
-            break;
-        }
-        else if (k == 2)
-        {
-            int t;
-            t = tinh_t();
-            printf("\nnhap mang cua a\n");
-            for (int i = t - 1; i >= 0; i--)
-            {
-                printf("a[%d] = ", i);
-                scanf("%lld", &a[i]);
-            }
-            printf("\nnhap mang cua b\n");
-            for (int i = t - 1; i >= 0; i--)
-            {
-                printf("b[%d] = ", i);
-                scanf("%lld", &b[i]);
-            }
-            tru_tren_f(k);
-            break;
-        }
-    }
+    printf("nhap a = ");
+    scanf("%lld", &d);
+    printf("nhap b = ");
+    scanf("%lld", &e);
+    tru_tren_f();
 }
 
 int tinh_t()
@@ -67,40 +31,34 @@ long long *bieu_dien_a(long long a)
     t = tinh_t();
     long long uoc, luythua;
     int j = t - 1;
-    // printf("cac he so la");
     for (int i = t - 1; i >= 0; i--)
     {
         luythua = pow(2, i * w);
         uoc = a / luythua;
         a = a % luythua;
         mang[j] = uoc;
-        // printf("\nhe so thu %d = %d,  %d", j + 1, uoc, mang[j]);
         j--;
     }
     return mang;
 }
 
-long long *tru_chinh_xac_boi(int k)
+long long *tru_chinh_xac_boi()
 {
     static long long c[100];
     long long hieu, luythua;
     int t, ep = 0;
     t = tinh_t();
-    if (k == 1)
+    long long *f, *g;
+    f = bieu_dien_a(d);
+    for (int i = 0; i < t; i++)
     {
-        long long *f, *g;
-        f = bieu_dien_a(d);
-        for (int i = 0; i < t; i++)
-        {
-            a[i] = *(f + i);
-        }
-        g = bieu_dien_a(e);
-        for (int i = 0; i < t; i++)
-        {
-            b[i] = *(g + i);
-        }
+        a[i] = *(f + i);
     }
-    // printf("\ncac he so va 'e' la");
+    g = bieu_dien_a(e);
+    for (int i = 0; i < t; i++)
+    {
+        b[i] = *(g + i);
+    }
     for (int i = 0; i <= t - 1; i++)
     {
         hieu = a[i] - b[i] - ep;
@@ -115,21 +73,19 @@ long long *tru_chinh_xac_boi(int k)
             ep = 0;
             c[i] = hieu;
         }
-        // printf("\nc[%d] = %d, e = %d", i, c[i], ep);
     }
     c[t] = ep;
     return c;
 }
-void tru_tren_f(int k)
+void tru_tren_f()
 {
     long long *f, c[100];
     int t;
     t = tinh_t();
-    f = tru_chinh_xac_boi(k);
+    f = tru_chinh_xac_boi();
     for (int i = 0; i <= t; i++)
     {
         c[i] = *(f + i);
-        // printf("\nc[%d] = %d", i, c[i]);
         if (i == t)
         {
             c[i] = *(f + t);
@@ -160,7 +116,6 @@ void cong_chinh_xac_boi(long long a[])
     for (int i = 0; i < t; i++)
     {
         b[i] = *(g + i);
-        // printf("\nb[%d] = %d, a[%d] = %d", i, b[i], i, a[i]);
     }
     printf("\ncac he so va 'e' la");
     for (int i = 0; i <= t - 1; i++)
